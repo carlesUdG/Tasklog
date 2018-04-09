11d90e25366bc3cd5adb0d49124ced3c501654cb
Carles Sallés <csallesbaro@gmail.com>
2018-04-10 01:00:57 +0200

    first files

---
+++ Tasklogs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasklogs
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> list = new List<string> { "1", "2", "E", "4" };
+
+            List<Tasklog> listTask = new List<Tasklog>();
+
+            foreach (var item in list)
+            {
+                Tasklog t = new Tasklog();
+                t.status = TaskStatus.pending;
+                t.item = item;
+
+                t.processItem();
+
+                listTask.Add(t);
+
+                t.showList(listTask);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tasklogs/tasklog.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasklogs
+{
+    enum TaskStatus
+    {
+        pending = 1,
+        completed = 2,
+        failed = 3
+    }
+
+    class Tasklog
+    {
+        public TaskStatus status { get; set; }
+
+        public Object item { get; set; }
+
+        public void processItem()
+        {
+            try
+            {
+                this.item = (Int32.Parse((string)this.item) + 1).ToString();
+                this.status = TaskStatus.completed;
+            }
+            catch (Exception)
+            {
+                this.status = TaskStatus.failed;
+            }
+        }
+
+        public void showList(List<Tasklog> tasklist)
+        {
+            foreach (var task in tasklist)
+            {
+                Console.Write("Task: " + task.item + " ==> ");
+
+                switch (task.status)
+                {
+                    case TaskStatus.pending:
+                        Console.WriteLine("PENDING");
+                        break;
+                    case TaskStatus.completed:
+                        Console.WriteLine("COMPLETED");
+                        break;
+                    case TaskStatus.failed:
+                        Console.WriteLine("FAILED");
+                        break;
+                    default:
+                        Console.WriteLine("UNKNOWN");
+                        break;
+                }
+            }
+        }
+    }
+}
